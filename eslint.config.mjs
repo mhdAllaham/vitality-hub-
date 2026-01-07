0ff9a5dc6a96213dcc5e81962463d1cf9f6d19a6
mhdAllaham <mhd.abo.khaleel@gmail.com>
2026-01-07 19:06:17 +0300

    Final ESLint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
 ];
 
 export default eslintConfig;
